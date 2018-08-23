e1ff482f719cace279ac175b44dd412c8bd7b37e
Clarke Stevens <clarke.stevens@sjrb.ca>
2018-08-22 23:20:05 -0600

    debugging

--- IoTivity/set_ocf_implementation.sh
+++ IoTivity/set_ocf_implementation.sh
@@ -27,6 +27,7 @@ if [[ ! -v OCFPATH ]]; then
   echo "export OCFPATH=~/$OCF_VERSION" >> ~/.bashrc
 
   export PATH=~/$OCF_VERSION:$PATH
+  echo "export PATH=~/OCF_VERSION:\$PATH" >> ~/.bashrc
 elif [ "$OCFPATH" == 'iot' ]; then
   sed -i.bak "s|"iot"|${OCF_VERSION}|g" ~/.bashrc
 else
